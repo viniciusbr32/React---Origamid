c179b4a37cdec636fa7f3f5f6d7681fcd379dcb7
viniciusbr32 <viniciusgba2008@gmail.com>
2024-06-22 20:05:59 -0300

    input like e id noticias

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -12,48 +12,49 @@ const App = () => {
       postedAT: '17 de ago, 2024',
     },
     {
-      id: 1,
+      id: 2,
       titulo: 'O que é linguagem de programação? Conheça as principais',
       conteudo:
         'Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.',
       postedAT: '17 de ago, 2024',
     },
     {
-      id: 1,
+      id: 3,
       titulo: 'O que é linguagem de programação? Conheça as principais',
       conteudo:
         'Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.',
       postedAT: '17 de ago, 2024',
     },
     {
-      id: 1,
+      id: 4,
       titulo: 'O que é linguagem de programação? Conheça as principais',
       conteudo:
         'Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.',
       postedAT: '17 de ago, 2024',
     },
     {
-      id: 1,
+      id: 5,
       titulo: 'O que é linguagem de programação? Conheça as principais',
       conteudo:
         'Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.',
       postedAT: '17 de ago, 2024',
     },
     {
-      id: 1,
+      id: 6,
       titulo: 'O que é linguagem de programação? Conheça as principais',
       conteudo:
         'Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.',
       postedAT: '17 de ago, 2024',
     },
     {
-      id: 1,
+      id: 7,
       titulo: 'O que é linguagem de programação? Conheça as principais',
       conteudo:
         'Uma das mais populares vertentes da tecnologia da informação, a área de programação segue tendo muita demanda de trabalho justamente pela velocidade com que dispositivos tecnológicos vêm avançando.',
       postedAT: '17 de ago, 2024',
     },
   ]);
+
   return (
     <>
       <Header />
--- first/src/Componentes/main/Noticias.jsx
+++ first/src/Componentes/main/Noticias.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Noticia from './Noticia';
 
 const Noticias = ({ noticias }) => {
